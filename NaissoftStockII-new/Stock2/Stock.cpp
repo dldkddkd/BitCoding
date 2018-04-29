66710e67a14337b98a14ca774ab1475e9245378c
kyungjecho <jkjk396@gmail.com>
2018-04-30 00:11:51 +0900

    Write comment
    주식에 관한 함수에 코맨트 남겼습니다.  do ~ while 문은 while 문으로 바꾸었습니다.

--- NaissoftStockII-new/Stock2/Stock.cpp
+++ NaissoftStockII-new/Stock2/Stock.cpp
@@ -5,6 +5,10 @@
 
 Stock *gHead, *gNow, gTmp;
 
+/** Initialize stock node
+*
+* @return void
+*/
 void InitStock()
 {
 	gHead = (Stock *)malloc(sizeof(Stock));
@@ -12,47 +16,86 @@ void InitStock()
 	gHead->prev = NULL;
 }
 
-Stock *InsertStock(Stock *Target, Stock *aStock)
+/** Insert stock next target node
+*
+* @param *pTarget
+* Where to insert
+*
+* @param *aStock
+* The node to insert
+*
+* @return Stock pointer
+* New node inserted
+*/
+Stock *InsertStock(Stock *pTarget, Stock *aStock)
 {
-	Stock *New;
-	Stock *Right;
+	Stock *pNew;
+	Stock *pRight;
 
-	New = (Stock *)malloc(sizeof(Stock));
-	*New = *aStock;
+	pNew = (Stock *)malloc(sizeof(Stock));
+	*pNew = *aStock;
 
-	Right = Target->next;
-	New->next = Right;
-	New->prev = Target;
+	pRight = pTarget->next;
+	pNew->next = pRight;
+	pNew->prev = pTarget;
 
-	Target->next = New;
-	if (Right) Right->prev = New;
-	return New;
+	pTarget->next = pNew;
+
+	if (pRight) 
+		pRight->prev = pNew;
+
+	return pNew;
 }
 
-bool DeleteStock(Stock *Target)
+/** Delete a target node
+*
+* @param *pTarget
+* Node to delete
+*
+* @return bool
+* If the target node exists, return true
+* Not exists, return false
+*/
+bool DeleteStock(Stock *pTarget)
 {
-	Stock *left, *right;
+	Stock *pLeft, *pRight;
 
-	if (Target == NULL || Target == gHead) return false;
+	if (pTarget == NULL || pTarget == gHead) 
+		return false;
 
-	left = Target->prev;
-	right = Target->next;
+	pLeft = pTarget->prev;
+	pRight = pTarget->next;
 
-	left->next = right;
-	if (right) right->prev = left;
+	pLeft->next = pRight;
+	if (pRight) pRight->prev = pLeft;
 
-	free(Target);
+	free(pTarget);
 	return true;
 }
 
+/** Add the node after the end
+* 사용되지 않습니다
+* @param *aStock
+* Node to add
+*
+* @return void
+*/
 void AppendStock(Stock *aStock)
 {
-	Stock *tail;
+	Stock *pTail;
 
-	for (tail = gHead; tail->next; tail = tail->next){}
-	InsertStock(tail, aStock);
+	for (pTail = gHead; pTail->next; pTail = pTail->next){}
+	InsertStock(pTail, aStock);
 }
 
+/** Find the node of the index
+*
+* @param idx
+* Location of the node user want to find
+*
+* @return Stock pointer
+* Found the Node or NULL
+*/
 Stock *FindStock(int idx)
 {
 	Stock *now;
@@ -60,13 +103,18 @@ Stock *FindStock(int idx)
 
 	for (now = gHead->next; now; now = now->next)
 	{
-		if (index == idx) return now;
+		if (index == idx) 
+			return now;
 		index++;
 	}
 	return NULL;
 }
 
-void UnInitStock()
+/** Deallocate all stock nodes
+*
+* @return void
+*/
+void DeallocateStock()
 {
 	while (DeleteStock(gHead)) { ; }
 
